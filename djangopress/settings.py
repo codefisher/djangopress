f7364e48b49af045ecf8f726369c0383dc80269e
Michael Buckley <codefisher@users.noreply.github.com>
2014-08-16 16:40:02 +0000

    fixing setings typo

--- djangopress/settings.py
+++ djangopress/settings.py
@@ -86,7 +86,7 @@ MIDDLEWARE_CLASSES = (
     #'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.common.CommonMiddleware',
     'djangopress.pages.middleware.PagesMiddleware',
-    'codefisher_apps.middleware.PagesMiddleware'
+    'codefisher_apps.middleware.PagesMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
