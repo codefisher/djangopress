e1376285f056233367fc53f430093173c6bc0388
Michael Buckley <codefisher@users.noreply.github.com>
2014-08-19 15:10:44 +0000

    updating fixtures

--- djangopress/dump.sh
+++ djangopress/dump.sh
@@ -1,8 +1,8 @@
-../manage.py dumpdata --indent 2 blog > ../fixtures/blog.json
-../manage.py dumpdata --indent 2 forum > ../fixtures/forum.json
-../manage.py dumpdata --indent 2 links menus > ../fixtures/links.json
-../manage.py dumpdata --indent 2 sites > ../fixtures/sites.json
-../manage.py dumpdata --indent 2 accounts auth > ../fixtures/auth.json
-../manage.py dumpdata --indent 2 pages feed_block > ../fixtures/pages.json
-../manage.py dumpdata --indent 2 downloads extension_downloads > ../fixtures/downloads.json
-../manage.py dumpdata --indent 2 donate ipn > ../fixtures/donate.json
+python ../manage.py dumpdata --indent 2 blog > ../fixtures/blog.json
+python ../manage.py dumpdata --indent 2 forum > ../fixtures/forum.json
+python ../manage.py dumpdata --indent 2 links menus > ../fixtures/links.json
+python ../manage.py dumpdata --indent 2 sites > ../fixtures/sites.json
+python ../manage.py dumpdata --indent 2 accounts auth > ../fixtures/auth.json
+python ../manage.py dumpdata --indent 2 pages feed_block > ../fixtures/pages.json
+python ../manage.py dumpdata --indent 2 downloads extension_downloads > ../fixtures/downloads.json
+python ../manage.py dumpdata --indent 2 donate ipn > ../fixtures/donate.json
