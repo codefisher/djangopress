f3c261727b842366d22a9e5b1b1afdc7ac28150c
Michael Buckley <codefisher@users.noreply.github.com>
2014-08-30 09:42:51 +0000

    removed menu and links apps, ready to make new ones

--- djangopress/core/links/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from djangopress.core.links.models import Link
-
-
-class LinkAdmin(admin.ModelAdmin):
-    list_display = ('label_text', 'location')
-
-admin.site.register(Link, LinkAdmin)
