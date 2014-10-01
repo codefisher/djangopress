46d80c48c8546e4cb6d2c01b893cc8df254b12f4
Michael Buckley <codefisher@users.noreply.github.com>
2014-10-01 22:09:53 +0000

    fixing crash in registration

--- djangopress/accounts/views.py
+++ djangopress/accounts/views.py
@@ -85,7 +85,8 @@ def register(request):
         if 'djangopress.iptools' in settings.INSTALLED_APPS:
             from djangopress.iptools.views import get_request_time_zone
             tzname = get_request_time_zone(request)
-            form = UserForm(timezone=tzname)
+            form = UserForm()
+            form.fields['timezone'].initial = tzname
         else:
             form = UserForm()
     data.update({
