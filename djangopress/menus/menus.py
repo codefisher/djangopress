4a61aaa244aafaed9f0e2fcb3dab36c8a7fcae54
codefisher <mail@codefisher.org>
2015-07-30 11:08:27 +1000

    Added new renderer, and made it possible to select renderer with the template tag.

--- djangopress/menus/menus.py
+++ djangopress/menus/menus.py
@@ -2,15 +2,14 @@ from djangopress.menus.menu import register
 from django.template import Template, RequestContext
 
 class ListRender(object):
-    def __init__(self):
-        self._menu = Template("""
+    _menu = Template("""
         {% load display_menu %}
         <ul{% if menu.class_tag%} class="{{ menu.class_tag }}"{% endif %}{% if menu.name %} id="{{ menu.name }}"{% endif %}>
             {% for item, sub_menu in tree %}
                 {% display_menu_item item sub_menu %}
             {% endfor %}
         </ul>""")
-        self._item = Template("""
+    _item = Template("""
             {% load display_menu %}
             <li {% if item.id_tag %} id="{{ item.id_tag }}" {% endif %}{% if item.class_tag %} class="{{ item.class_tag }}"{% endif %}>
                 <a href="{{ item.link }}">{{ item.label }}</a>
@@ -29,13 +28,35 @@ class ListRender(object):
         except:
             pass
         
-    def render_menu(self, context, tree, menu=None):
+    def render_menu(self, context, tree, menu=None, renderer=None):
         tree = sorted(((item, sub_menu) for item, sub_menu in tree.items()), key=lambda x: x[0].index)
         self._tag_active_item(context, tree)
-        return self._menu.render(RequestContext(context.get("request"), {"menu": menu, "tree": tree}))
+        return self._menu.render(RequestContext(context.get("request"), {"menu": menu, "tree": tree, 'renderer': renderer}))
     
     def render_item(self, context, item, sub_menu):
         return self._item.render(RequestContext(context.get("request"), {"item": item, "sub_menu": sub_menu}))
     
-    
+
 register('default', ListRender())
+
+class HeadingRender(ListRender):
+
+    _menu = Template("""
+        {% load display_menu %}
+        <div{% if menu.class_tag%} class="{{ menu.class_tag }}"{% endif %}>
+            {% for item, sub_menu in tree %}
+                {% display_menu_item item sub_menu renderer %}
+            {% endfor %}
+        </div>""")
+    _item = Template("""
+            {% load display_menu %}
+            <div class="heading-menu">
+            <h2 {% if item.id_tag %} id="{{ item.id_tag }}" {% endif %}{% if item.class_tag %} class="{{ item.class_tag }}"{% endif %}>
+                <a href="{{ item.link }}">{{ item.label }}</a></h2>
+                {% if sub_menu %}
+                    {% display_submenu item sub_menu %}
+                {% endif %}
+                </div>
+            """)
+
+register('headings', HeadingRender())
--- djangopress/menus/templatetags/display_menu.py
+++ djangopress/menus/templatetags/display_menu.py
@@ -5,7 +5,7 @@ from djangopress.menus.models import Menu, MenuItem
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
-def display_menu(context, menu_name):
+def display_menu(context, menu_name, renderer=None):
     menu = Menu.objects.get(name=menu_name)
     menu_items = MenuItem.objects.filter(menu=menu).select_related('parent')
     nodes = dict((item, {}) for item in menu_items)
@@ -14,14 +14,14 @@ def display_menu(context, menu_name):
             nodes[item.parent][item] = nodes[item]
     tree = dict((key, value) for key, value in nodes.items() if key.parent is None)
     try:
-        return menu_register.get_renderer(menu.renderer).render_menu(context, tree, menu)
+        return menu_register.get_renderer(renderer if renderer else menu.renderer).render_menu(context, tree, menu, renderer)
     except:
         return ""
 
 @register.simple_tag(takes_context=True)
-def display_menu_item(context, item, submenu):
-    return menu_register.get_renderer(item.renderer).render_item(context, item, submenu)
+def display_menu_item(context, item, submenu, renderer=None):
+    return menu_register.get_renderer(renderer if renderer else item.renderer).render_item(context, item, submenu)
 
 @register.simple_tag(takes_context=True)
-def display_submenu(context, item, submenu):
-    return menu_register.get_renderer(item.renderer).render_menu(context, submenu) 
+def display_submenu(context, item, submenu, renderer=None):
+    return menu_register.get_renderer(renderer if renderer else item.renderer).render_menu(context, submenu)
