fa1ab76fc61b5a6cf392c07d0fd0d790ddb2956d
codefisher <mail@codefisher.org>
2017-07-03 18:24:59 +1000

    Adding mail log to Admin

--- djangopress/contact/admin.py
+++ djangopress/contact/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import MailAddress
+from .models import MailAddress, MailLog
 
 class MailAddressAdmin(admin.ModelAdmin):
     list_display = ('name', 'email')
 
 admin.site.register(MailAddress, MailAddressAdmin)
+
+class MailLogAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'name', 'email')
+
+admin.site.register(MailLog, MailLogAdmin)
